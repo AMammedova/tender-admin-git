10cfbe819806427bc3d7fea5c6dd321143a2ae2c
AMammedova <ayselmemmedova1718@gmail.com>
2025-05-16 16:04:25 +0400

    change next auth url

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const nextConfig = {
-    images: {
-        domains: [
-          'tend.grandmart.az',
-        ],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    }
+  images: {
+    domains: ["tend.grandmart.az"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  trustHost: true,
 };
- 
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
